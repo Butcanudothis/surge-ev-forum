ea59bbe7463934d33c604e50828513a4f010e476
Butcanudothis <akshayvarma.varma@gmail.com>
2023-04-13 22:47:35 +1000

    implemented form validation and state management for the form

--- src/App.js
+++ src/App.js
@@ -43,40 +43,40 @@ const initialFacts = [
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [posts, setPosts] = useState(initialFacts);
+
   return (
     <>
       {/* header */}
-      <Header showForm = {showForm} setShowForm = {setShowForm}/>
+      <Header showForm={showForm} setShowForm={setShowForm} />
 
-      {showForm ? <NewPostForm /> : null}
+      {showForm ? <NewPostForm setPosts={setPosts} setShowForm={setShowForm} /> : null}
 
       <main className="main">
         <CategoryFilter />
-        <PostList />
+        <PostList posts={posts}/>
       </main>
       <Footer />
     </>
   );
 }
 
-function Header({showForm, setShowForm}) {
-return <header className="header">
-<div className="logo">
-  <img src="logo.png" alt="Surge EV logo" />
-  <h1>Surge EV Updates</h1>
-</div>
+function Header({ showForm, setShowForm }) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Surge EV logo" />
+        <h1>Surge EV Updates</h1>
+      </div>
 
-<button
-  className="btn btn-large shareFactButton"
-  onClick={() => setShowForm((show) => !show)}
->
-  {showForm ? "Close" : "Share an update"}
-</button>
-</header>
-}
-
-function NewPostForm() {
-  return <form className="factForm"> Post Form</form>;
+      <button
+        className="btn btn-large shareFactButton"
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Share an update"}
+      </button>
+    </header>
+  );
 }
 const CATEGORIES = [
   { name: "Motor tech", color: "#f87171" },
@@ -95,6 +95,105 @@ const CATEGORIES = [
   { name: "Investor relations", color: "#dc2626" },
   { name: "Government updates", color: "#059669" },
 ];
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+
+function NewPostForm({setPosts , setShowForm}) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+  const [source, setSource] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submit");
+    // check if data is valid
+   if (text && category && isValidHttpUrl(source) && isValidHttpUrl(image) && text.length <= 200) {
+    const newPost = {
+      id: Math.floor(Math.random() * 1000),
+      text: text,
+      source: source,
+      category: category,
+      votesInteresting: 0,
+      votesMindblowing: 0,
+      votesFalse: 0,
+      createdIn: new Date().getFullYear(),
+      image_src: image,
+    };
+
+    // create new post object
+    // add new post to the UI: add the post to state
+   setPosts((posts) => [newPost, ...posts]);
+    // reset input fields
+    setText("");
+    setCategory("");
+    setSource("");
+    setImage("");
+
+    // close the form
+    setShowForm(false);
+
+
+  }
+}
+
+  return (
+    <form className="factForm" onSubmit = {handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Share your Thoughts"
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
+      <span style={
+        text.length > 200 ? { color: "red" } : { color: "white" }
+      }>{200 -text.length} characters</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source"
+        value={source}
+        onChange={(event) => {
+          setSource(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Image source"
+        value={image}
+        onChange={(event) => {
+          setImage(event.target.value);
+        }}
+      />
+      <select
+        value={category}
+        onChange={(event) => {
+          setCategory(event.target.value);
+        }}
+      >
+        <option value="">Choose Category</option>
+        {CATEGORIES.map((category) => (
+          <option key={category.name} value={category.name}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
+
 function CategoryFilter() {
   return (
     <aside>
@@ -121,8 +220,7 @@ function getCategoryColor(categoryName) {
   return category ? category.color : "#121212";
 }
 
-function PostList() {
-  const posts = initialFacts;
+function PostList({ posts }) {
   return (
     <section>
       <ul className="postList">
@@ -162,10 +260,12 @@ function Post({ post }) {
   );
 }
 function Footer() {
- return <footer>
- <p>© 2023 Surge EV. All rights reserved.</p>
- <p>built with ❤️ by Akshay Varma</p>
-</footer>
+  return (
+    <footer>
+      <p>© 2023 Surge EV. All rights reserved.</p>
+      <p>built with ❤️ by Akshay Varma</p>
+    </footer>
+  );
 }
 
 export default App;
